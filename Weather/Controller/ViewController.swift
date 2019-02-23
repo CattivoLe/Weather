cff638bb74d08d90eed699276353aa08a4960895
Alexander Omelchuk <42239507+CattivoLe@users.noreply.github.com>
2019-02-23 13:03:30 +0300

    Аутлеты

--- Weather/Controller/ViewController.swift
+++ Weather/Controller/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var appearentTemperature: UILabel!
+    @IBOutlet weak var  pressureLabel: UILabel!
+    @IBOutlet weak var  humidityLabel: UILabel!
+    @IBOutlet weak var refrashButton: UIButton!
+    
+    @IBAction func refreshButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
