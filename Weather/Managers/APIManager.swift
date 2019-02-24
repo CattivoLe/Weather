3d7243053b595800dfabb8bd25fcae371459cc39
Alexander Omelchuk <42239507+CattivoLe@users.noreply.github.com>
2019-02-24 15:37:43 +0300

    APIManager Первый дефолтный метод

--- Weather/Managers/APIManager.swift
+++ Weather/Managers/APIManager.swift
@@ -25,6 +25,46 @@ protocol APIManager {
     func fetch<Type> (request: URLRequest, parse: ([String:AnyObject]) -> Type?, completionHandler: (APIResult<Type>) -> Void )
     
     init (sessionConfiguration: URLSessionConfiguration)
+}
+
+extension APIManager {
+    
+    func JSONTaskWith (request: URLRequest, completionHandler: @escaping JSONCompletionHandler) -> JSONTask {
+        
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            
+            guard let HTTPResponse = response as? HTTPURLResponse else {
+                
+                let userInfo = [NSLocalizedDescriptionKey: NSLocalizedString("Missing HTTP Response", comment: "MissingHTTP")]
+                let error = NSError(domain: WETNetworkingErrorDomain, code: 100, userInfo: userInfo)
+                
+                completionHandler(nil, nil, error)
+                return
+            }
+            
+            if data == nil {
+                if let error = error {
+                    completionHandler(nil, HTTPResponse, error)
+                }
+            } else {
+                switch HTTPResponse.statusCode {
+                case 200:
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String:AnyObject]
+                        completionHandler(json, HTTPResponse, nil)
+                    } catch let error as NSError {
+                        completionHandler(nil, HTTPResponse, error)
+                    }
+                default: print("We have got response status \(HTTPResponse.statusCode)")
+                }
+            }
+        }
+        return dataTask
+    }
+    
+    func fetch<Type> (request: URLRequest, parse: ([String:AnyObject]) -> Type?, completionHandler: (APIResult<Type>) -> Void ) {
+        
+    }
     
     
 }
---
+++ Weather/Managers/ErrorManager.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorManager.swift
+//  Weather
+//
+//  Created by Alexander Omelchuk on 24/02/2019.
+//  Copyright © 2019 Александр Омельчук. All rights reserved.
+//
+
+import Foundation
+
+public let WETNetworkingErrorDomain = "Cattivole.WeatherApp.NetworkingError"
+public let MissingHTTPResponseError = 100
